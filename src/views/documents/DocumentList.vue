55c2d105b2e646ef024f4ae88c8c7ad7e9934032
yangchangling <1819719608@qq.com>
2026-05-27 16:33:17 +0800

    fix: 文档列表点击文件名改为跳转至内容浏览页面
    文件名点击原为 openRawFile() 打开新窗口显示原始文件，
    改为导航至 DocumentContent 页面展示清洗后的 Markdown 内容，
    用户可通过该页面的"查看原文件"按钮查看原始文件

--- src/views/documents/DocumentList.vue
+++ src/views/documents/DocumentList.vue
@@ -40,7 +40,7 @@
       <el-table-column type="selection" width="50" align="center" />
       <el-table-column label="文件名称" min-width="200">
         <template #default="{ row }">
-          <el-button link type="primary" size="small" @click="openRawFile(row)">
+          <el-button link type="primary" size="small" @click="$router.push(`/documents/${row.id}/content`)">
             {{ row.fileName }}
           </el-button>
         </template>
